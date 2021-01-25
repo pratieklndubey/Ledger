8fc8461958d627d251148cb0879c3336470ad032
prateek <prateeklndubey@gmail.com>
2021-01-25 12:27:04 +0530

    Made changes to node js files, to create layouts

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts')
 const port = 3000;
-hostname = 'localhost';
+const hostname = 'localhost';
 
-// Define the static file path
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index', {name: 'Prateek'});
 })
 
 app.listen(port,hostname, () => { console.log(`Server running at http://${hostname}:${port}/`);});
