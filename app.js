ecb52e06e5ba7ac2c23ca34823a14fe8e0f7e9b8
prateek <prateeklndubey@gmail.com>
2021-01-25 13:32:23 +0530

    Added mongodb using mongoose

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static(__dirname+'/public'));
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Ledge', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to the Database'));
+
 app.use('/', indexRouter)
 
 app.listen(port,hostname, () => { console.log(`Server running at http://${hostname}:${port}/`);});
