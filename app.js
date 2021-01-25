e0efdff7df360acc0543aead56086e7986506221
prateek <prateeklndubey@gmail.com>
2021-01-25 12:58:44 +0530

    Made changes to the routes file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const expressLayouts = require('express-ejs-layouts')
 const port = 3000;
 const hostname = 'localhost';
 
+const indexRouter = require('./routes/index')
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
@@ -11,8 +13,6 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static(__dirname+'/public'));
 
-app.get('/', function (req, res) {
-  res.render('index', {name: 'Prateek'});
-})
+app.use('/', indexRouter)
 
 app.listen(port,hostname, () => { console.log(`Server running at http://${hostname}:${port}/`);});
