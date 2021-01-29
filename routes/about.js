49606c86dd35cca8ff062bdfb09c78554f4224d8
prateek <prateeklndubey@gmail.com>
2021-01-29 13:42:51 +0530

    added the about page

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index', {name: 'Prateek'});
+router.get('/',  (req, res) => {
+    res.render('about/index', {name: 'Prateek'});
   })
 
 module.exports = router
