494ff76d7d98dc886f4ae89885ebf1b8d500af9f
prateek <prateeklndubey@gmail.com>
2021-02-05 02:52:08 +0530

    Minor changes to the repo and the first version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lEdge
 lEdge is something we all need. I always wanted to manage my money and analyse my income & expenses, but never really got an app with all the required features. So, I thought of creating this app with the features one require to manage their money seamlessly. I hope you would love it.
+
+![alt text](https://github.com/Sh0onya/Ledge/blob/main/Account.png?raw=true)
+![alt text](https://github.com/Sh0onya/Ledge/blob/main/Ledger.png?raw=true)
--- routes/account.js
+++ routes/account.js
@@ -25,30 +25,11 @@ router.get('/:id', async (req, res) => {
         let account = await Account.findById(req.params.id)
         let transaction = account.activity
         checkExpense = categoriesIncome.includes(req.body.category)
-        actualAmount = (checkExpense?req.body.amount:req.body.amount*-1)
+        actualAmount = (checkExpense?req.body.amount*1.00:req.body.amount*-1.00)
         let newTransaction = {title: req.body.title, amount: actualAmount, category: req.body.category, description: req.body.description, isexpense: !checkExpense, postranbal: (account.transum+actualAmount)}
         account.activity.push(newTransaction)
         account.transum = account.transum+actualAmount
         //res.send(newTransaction)
-        /*let entry = transaction.find(entry => entry._id == req.params.tid)
-        entry.description = req.body.description
-        entry.postranbal = entry.postranbal - entry.amount
-        entry.amount = req.body.amount
-        entry.title = req.body.title
-        entry.category = req.body.category
-        checkExpense = categoriesIncome.includes(entry.category)
-        if(checkExpense){
-          entry.isexpense = false
-        }
-        else{
-          entry.isexpense = true
-        }
-        if(entry.isexpense)
-        {
-          entry.amount *= -1.00
-        }
-        entry.postranbal = entry.postranbal + entry.amount
-        account.transum = entry.postranbal*/
         await account.save()
         res.redirect("../"+req.params.id)
       }
