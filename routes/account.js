2bc2738dcd5abe8be8c2386f5a3c486acfc6cc0f
prateek <prateeklndubey@gmail.com>
2021-03-09 18:10:11 +0530

    Minor Update

--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Account = require('../models/account')
 const fs = require('fs')
-const { reset } = require('nodemon')
+
 
 router.get('/', async (req, res) => {
   res.redirect('../')
