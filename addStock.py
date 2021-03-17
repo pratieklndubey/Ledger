4df28d9788cfffe43fec7ebec079d139de397007
prateek <prateeklndubey@gmail.com>
2021-03-17 22:54:55 +0530

    Updated python script to handle missing files

--- addStock.py
+++ addStock.py
@@ -1,6 +1,7 @@
 from nsetools import Nse
 import csv
 import sys
+from os import system,path
 nse = Nse()
 
 def listToString(value):  
@@ -14,19 +15,36 @@ if sys.argv[1] == 'NIFTYBEES' or sys.argv[1] == 'ICICIB22':
 
 if(validated):
     print("True")
-    flag = True
-    with open("stocks.csv", 'r') as checkFile:
-        reader = csv.reader(checkFile)
-        for row in reader:
-            if (sys.argv[1] == listToString(row)):
-                flag = False
-    
-    if (flag == True):
+    exists = path.exists('stocks.csv')
+    if exists:
+        flag = True
+        with open("stocks.csv", 'r') as checkFile:
+            reader = csv.reader(checkFile)
+            for row in reader:
+                if (sys.argv[1] == listToString(row)):
+                    flag = False
+
+        if (flag == True):
+            item = [sys.argv[1]]
+            with open('stocks.csv', 'a', newline='') as file:
+                appender = csv.writer(file)
+                appender.writerow(item)
+            with open('stockData.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                if sys.argv[1] == ['NIFTYBEES']:
+                    outPut = ['NIFTYBEES',157.75]
+                elif sys.argv[1] == ['ICICIB22']:
+                    outPut = ['ICICIB22',36.45]
+                else:
+                    ticker = nse.get_quote(sys.argv[1])
+                    outPut = [ticker['symbol'],ticker['lastPrice']]
+                writer.writerow(outPut)
+    else:
         item = [sys.argv[1]]
-        with open('stocks.csv', 'a', newline='') as file:
+        with open('stocks.csv', 'w', newline='') as file:
             appender = csv.writer(file)
             appender.writerow(item)
-        with open('stockData.csv', 'a', newline='') as file:
+        with open('stockData.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             if sys.argv[1] == ['NIFTYBEES']:
                 outPut = ['NIFTYBEES',157.75]
