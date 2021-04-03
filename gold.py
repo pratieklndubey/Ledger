7ef67fd322ff1fb5614ad87bd0dcae4f4b09c4f8
prateek <prateeklndubey@gmail.com>
2021-04-03 14:56:08 +0530

    Made changes to update gold & silver prices once in a day only, as we are using webscrapping.

--- gold.py
+++ gold.py
@@ -1,16 +1,23 @@
 import bs4 
 import requests 
+import pathlib
+import datetime
 
-url = 'https://www.goodreturns.in/gold-rates/delhi.html'
-result = requests.get(url) 
-soup = bs4.BeautifulSoup(result.content,'html.parser') 
-price = soup.find_all('strong', {"id":"el"})
-show = ""
-for pr in price:
-	show = pr.text
-show = show.translate({ord(' '):None,ord('₹'):None,ord(','):None})
-price = open('goldPrice.txt','w')
-price.write(show)
-price.close()
+fname = pathlib.Path('goldPrice.txt')
+mtime = datetime.datetime.fromtimestamp(fname.stat().st_mtime).date()
+today = datetime.date.today()
+print(mtime == today)
+if today != mtime:
+	url = 'https://www.goodreturns.in/gold-rates/delhi.html'
+	result = requests.get(url) 
+	soup = bs4.BeautifulSoup(result.content,'html.parser') 
+	price = soup.find_all('strong', {"id":"el"})
+	show = ""
+	for pr in price:
+		show = pr.text
+	show = show.translate({ord(' '):None,ord('₹'):None,ord(','):None})
+	price = open('goldPrice.txt','w')
+	price.write(show)
+	price.close()
 
 
