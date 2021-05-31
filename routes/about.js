c0c950d58b75a183a332e2d9f449c429b30a121b
prateek <prateeklndubey@gmail.com>
2021-05-31 16:55:07 +0530

    Updated and added icons

--- routes/about.js
+++ routes/about.js
@@ -6,7 +6,7 @@ const spawn = require('child_process').spawn
 const util = require('util')
 
 router.get('/', async (req, res) => {
-  res.render('about/index', {title: 'About',relative:'../',option:"",search:"",bell:"",searchRelative:'',calculate:"",});
+  res.render('about/index', {title: 'About',relative:'../',option:"",search:"",bell:"",searchRelative:'',calculate:"",god:"shivaji"});
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Account = require('../models/account')
 
 router.get('/', async (req, res) => {
     const account = await Account.find()
-    res.render('index', {title: 'Home', newaccount: new Account(), account: account, calculate:"", option: '',search:"",bell:"",relative:'',searchRelative:''});
+    res.render('index', {title: 'Home', newaccount: new Account(), account: account, calculate:"", option: '',search:"",bell:"",relative:'',searchRelative:'',god:"durgamata"});
   })
 
 module.exports = router
