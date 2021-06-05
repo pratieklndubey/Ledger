bc5034712796152c0b745d023d5e2a63ad863c32
prateek <prateeklndubey@gmail.com>
2021-06-05 23:36:16 +0530

    Added Notification addition feature

--- routes/about.js
+++ routes/about.js
@@ -6,7 +6,7 @@ const spawn = require('child_process').spawn
 const util = require('util')
 
 router.get('/', async (req, res) => {
-  res.render('about/index', {title: 'About',relative:'../',option:"",search:"",bell:"",searchRelative:'',calculate:"",god:"shivaji"});
+  res.render('about/index', {title: 'About',relative:'../',option:"",search:"",bell:"",searchRelative:'',calculate:"",god:"shivaji",noticount:""});
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Account = require('../models/account')
 
 router.get('/', async (req, res) => {
     const account = await Account.find()
-    res.render('index', {title: 'Home', newaccount: new Account(), account: account, calculate:"", option: '',search:"",bell:"",relative:'',searchRelative:'',god:"durgamata"});
+    res.render('index', {title: 'Home', newaccount: new Account(), account: account, calculate:"", option: '',search:"",bell:"",relative:'',searchRelative:'',god:"durgamata",noticount:""});
   })
 
 module.exports = router
