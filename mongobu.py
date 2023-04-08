ef09cf411396e0a8859963ab5abeea4601351016
Prateek Dubey <prateeklndubey@gmail.com>
2023-04-08 12:37:31 +0530

    Added backup code, will merge on UI later

--- mongobu.py
+++ mongobu.py
@@ -1,5 +1,4 @@
-"""from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive"""
+
 import shutil
 from datetime import date,datetime
 
@@ -20,38 +19,6 @@ dir_name = year+month+day
 
 shutil.make_archive(output_filename, 'zip', dir_name)
 
-shutil.rmtree(dir_name)
+#shutil.rmtree(dir_name)
 
-"""
-read_date = open('lastbudate.txt','r')
-performed = read_date.read()
-read_date.close()
-
-performed_list = list(performed.split("-"))
-performed_list = list(map(int,performed_list))
-
-performed_date = date(performed_list[0],performed_list[1],performed_list[2])
-
-current_date = date.today()
-
-date_difference = current_date - performed_date
-
-if int(date_difference.days) > 5:
-    lastbudate = year+"-"+month+"-"+day
-
-    write_date = open('lastbudate.txt','w')
-    write_date.write(lastbudate)
-    write_date.close()
-
-    
-    gauth = GoogleAuth()           
-    drive = GoogleDrive(gauth)  
-
-    upload_file_list = [output_filename+".zip"]
-    for upload_file in upload_file_list:
-        gfile = drive.CreateFile({'parents': [{'id': '18SfHg1rz9tXxKbwhd5_VNI8N0Y-iKKvi'}]})
-        # Read file and set it as the content of this instance.
-        gfile.SetContentFile(upload_file)
-        gfile.Upload() # Upload the file.
- """   
 
